8e7612bbab5c5e54d519a33e75323ce598569adb
panxiaoliang <panxiaoliang>
2024-10-26 18:46:34 +0800

    feat: 修改vite.config.ts 配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://xiaoliangpan.github.io/react-playground/",
+  // base: "https://xiaoliangpan.github.io/react-playground/",
+  // base: "./react-playground/",
   plugins: [react()],
 });
